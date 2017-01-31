59ed2bb596e4dc0085c08b29523cd890b348cd61
Wanvipa <kimonogirl_pa_41@hotmail.com>
2017-01-31 18:18:54 +0700

    31/1/60

--- src/components/SportCard.vue
+++ src/components/SportCard.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class=""><br>
     <div  class ="width-card">
-        <div class="card-content">
+        <div v-show="list.sport === 'Badminton' || list.sport === 'Table Tennis' ||  list.sport === 'Volleyball'" class="card-content">
           <div class="media">
             <div class="media-content">
               <div class="tap">
@@ -21,9 +21,47 @@
               </div>
             </div>
           </div>
+          <div v-show="list.sport === 'Basketball'" class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <div class="tap">
+                  <p class="title is-5 textsport" >
+                    <img src="./sport.png" class="tapstatus" alt="">: {{list.sport}} ( {{list.status}} )<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small class="sizedate">{{fromNow}}</small>
+                  </p>
+                </div>
+                </div>
+            </div>
+            <div class="content"><br>
+              <h5 class="subtitle is-5 ">{{list.competition}}</h5>
+              <div class="" v-for="score in scoreSports" v-if="score.sportId === list.id && score.status">ประเภท : {{list.kind}}
+                <h2 class="title is-2 textred ">{{score.team1}} - {{score.team2}}</h2>
+                
+                <a :href="list.locationLink"><h5 class="subtitle is-5 texttype "><i class="fa fa-map-marker location" aria-hidden="true"></i> : {{list.location}}</a>
+                </div>
+              </div>
+            </div>
+            <div v-show="list.sport === 'Football'" class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <div class="tap">
+                    <p class="title is-5 textsport" >
+                      <img src="./sport.png" class="tapstatus" alt="">: {{list.sport}} ( {{list.status}} )<br>
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small class="sizedate">{{fromNow}}</small>
+                    </p>
+                  </div>
+                  </div>
+              </div>
+              <div class="content"><br>
+                <h5 class="subtitle is-5 ">{{list.competition}}</h5>
+                <div class="" v-for="score in scoreSports" v-if="score.sportId === list.id && score.status">
+                  <h2 class="title is-2 textred ">{{score.team1}} - {{score.team2}}</h2>
+                  <a :href="list.locationLink"><h5 class="subtitle is-5 texttype "><i class="fa fa-map-marker location" aria-hidden="true"></i> : {{list.location}}</a>
+                  </div>
+                </div>
+              </div>
         </div>
-
-  </div>
+      </div>
 </template>
 
 <script>
